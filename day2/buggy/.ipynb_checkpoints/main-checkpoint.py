a6fbb03f652b8fe9f80cb2a722824b1b9dff9801
Leonardo Monrroy <leonardo.monrroy@kemi.uu.se>
2023-03-07 16:36:00 +0100

    Fixed buggy dice game

---
+++ day2/buggy/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,12 @@
+from dicegame.runner import GameRunner
+
+
+def main():
+    print("Add the values of the dice")
+    print("It's really that easy")
+    print("What are you doing with your life.")
+    GameRunner.run()
+import ipdb; ipdb.set_trace()
+
+if __name__ == "__main__":
+    main()
